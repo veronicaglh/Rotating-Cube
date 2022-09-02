7b784aea53db9394f6a1e40217b41a5376f99387
veronicaglh <veronicagt.IT@gmail.com>
2022-09-02 20:21:38 +0300

    draw pygame window

--- cube.py
+++ cube.py
@@ -74,3 +74,25 @@ def Cube():
             glVertex3fv(vertices[vertex])
 
     glEnd()
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        glRotatef(0, 0, 0, 0)
+        glRotatef(1, 3, 1, 1)
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
