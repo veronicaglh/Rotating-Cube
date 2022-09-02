84061f66e79c0c2bf69b55e991cec7711c9ce26b
veronicaglh <veronicagt.IT@gmail.com>
2022-09-02 20:09:42 +0300

    first commit

---
+++ cube.py
@@ -0,0 +1,12 @@
+# This program will build a rotating cube using python
+# For this program to work some packages will need to be installed
+# In order to install these packages you can run this command on the terminal of your IDE
+# pip install pygame
+# pip install numpy
+
+# Import statements
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np
